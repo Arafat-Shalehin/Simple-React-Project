cbe73a945e80f33bbc2607b02b31549d1751f3b8
Arafat Shalehin <md.arafatshalehin@gmail.com>
2025-09-18 20:53:15 +0600

    Basic Setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Countries from './Components/Countries/Countries'
+
+function App() {
+  return (
+    <>
+      <Countries></Countries>
+    </>
+  )
+}
+
+export default App
