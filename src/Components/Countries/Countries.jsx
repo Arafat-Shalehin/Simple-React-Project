a5317b0ec467b698b845bedfb5ea44f7e263673c
Arafat Shalehin <md.arafatshalehin@gmail.com>
2025-09-19 10:35:57 +0600

    Added some functionalities

--- src/Components/Countries/Countries.jsx
+++ src/Components/Countries/Countries.jsx
@@ -1,24 +1,46 @@
-import { use } from "react";
+import { use, useState } from "react";
 import Country from "../Country/Country";
 
 const Countries = ({countriesPromise}) => {
+
+    const [visitedCountries, setVisitedCountries] = useState([]);
+
+    const handleVisitedCountries = (country) => {
+        const newVisitedCountries = [...visitedCountries, country];
+        setVisitedCountries(newVisitedCountries);
+    }
 
     const countriesData = use(countriesPromise);
 
     const countries = countriesData.countries;
 
     return(
-        <div>
+        <div className="w-[1250px] mx-auto">
             <h1 className="text-5xl text-center my-10">In the Countries: { countries.length}
                 <br />How many have you visited?
             </h1>
 
-            <div className="grid grid-cols-3 gap-10">
+            <h3 className="text-3xl font-bold my-10">
+                Total Countries visited: {visitedCountries.length}
+            </h3>
+
+            <div>
+                <ol className="list-decimal list-inside">
+                    {
+                        visitedCountries.map(country => 
+                        <li className="font-semibold text-xl text-left" key={country.cca3.cca3}>{country.name.common}
+                        </li>)
+                    }
+                </ol>
+            </div>
+
+            <div className="grid grid-cols-3 gap-10 mt-5">
                 {
                     countries.map(country => 
                     <Country
-                        key={country.cca3.cca3}
-                        country={country}>
+                        key = {country.cca3.cca3}
+                        country = {country}
+                        handleVisitedCountries = {handleVisitedCountries}>
                     </Country>)
                 }
             </div>
--- src/Components/Country/Country.jsx
+++ src/Components/Country/Country.jsx
@@ -1,21 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
+import "./Country.css"
 
-const Country = ({country}) => {
+const Country = ({country, handleVisitedCountries}) => {
 
-    console.log(country);
+    const [visited, setVisited] = useState(false);
 
-    console.log(country.name.common);
+    const handleVisited = () => {
+        setVisited(!visited);
+        
+        if (!visited) {
+            handleVisitedCountries(country);
+        }
+    }
 
     return (
-        <div className='w-100 border-2 border-orange-400 rounded-xl mx-auto text-center'>
+        <div className=
+        {`w-100 border-2 border-amber-500 rounded-xl mx-auto text-center 
+        ${visited && 'country-visited'}`}>
 
-            <img className='mt-7 mx-auto' src={country.flags.flags.png} alt={country.flags.flags.alt} />
+            <img className='mt-7 mx-auto max-h-[160px]' src={country.flags.flags.png} alt={country.flags.flags.alt} />
 
             <h3 className='mt-3 font-bold text-2xl'>Name: {country.name.common}</h3>
 
             <p className='font-semibold'>Population: {country.population.population}</p>
 
             <p className='font-medium'>Continent: {country.continents.continents}</p>
+
+            <button onClick={handleVisited} className='btn px-8 py-4 rounded-xl text-2xl font-bold 
+            bg-green-600 mb-5 mt-5'>
+                {visited ? "Visited" : "Not Visited"}
+            </button>
 
         </div>
 
